4403df4ed0565731f0e6e8a81083e6f404a1263d
Faraz Patankar <farazpatankar@gmail.com>
2024-06-22 12:25:22 +0400

    remove backticks in code blocks

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,6 +76,18 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      typography: _theme => ({
+        DEFAULT: {
+          css: {
+            "code::before": {
+              content: "",
+            },
+            "code::after": {
+              content: "",
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
